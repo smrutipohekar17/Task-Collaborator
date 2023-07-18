90ca0601a6ad8116ce3a1f141454ac8cffde0679
Smruti Pohekar <smrutipohekar17@gmail.com>
2023-07-18 15:09:56 +0530

    ok now

--- server/server.js
+++ server/server.js
@@ -10,12 +10,20 @@ const projectsRoute = require("./routes/projectsRoute");
 const tasksRoute = require("./routes/tasksRoute");
 const notificationsRoute = require("./routes/notificationsRoute");
 
-
 app.use("/api/users", usersRoute);
 app.use("/api/projects", projectsRoute);
 app.use("/api/tasks", tasksRoute);
 app.use("/api/notifications", notificationsRoute);
 
-app.listen(port, () =>
-  console.log(`Node JS Server listening on port ${port}`)
-);
+// deployment config
+const path = require("path");
+__dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
+
+app.listen(port, () => console.log(`Node JS server listening on port ${port}`));
